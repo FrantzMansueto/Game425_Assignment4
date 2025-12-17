f1059d72169011516108a9d9e102245a02bec9a2
GiraffeA77 <mrust1@arizona.edu>
2025-12-16 22:26:15 -0700

    script cleanup

--- Game425_Assignmento4/Assets/Scripts/HeroAnimator.cs
+++ Game425_Assignmento4/Assets/Scripts/HeroAnimator.cs
@@ -8,27 +8,19 @@ public class HeroAnimator : MonoBehaviour
 
     void Update()
     {
-        // Get input
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
         moveDirection = new Vector3(h, 0, v);
 
-        // Movement
         if (moveDirection.magnitude > 0.1f)
         {
-            // Move hero
             transform.Translate(moveDirection.normalized * moveSpeed * Time.deltaTime, Space.World);
-
-            // Rotate hero to face direction
             Quaternion targetRotation = Quaternion.LookRotation(moveDirection);
             transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 10f);
-
-            // Animator parameters
             animator.SetFloat("Speed", moveDirection.magnitude);
         }
         else
         {
-            // Idle
             animator.SetFloat("Speed", 0f);
         }
     }
